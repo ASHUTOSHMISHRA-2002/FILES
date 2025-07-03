9b6fe93a3c48905906c71cee63c2c324ce3f6457
Ashutosh Mishra <120199306+ASHUTOSHMISHRA-2002@users.noreply.github.com>
2025-07-03 12:05:57 +0530

    Delete JUnit_Demo_Project directory

--- JUnit_Demo_Project/src/com/mockito/AddService.java
+++
@@ -1,7 +0,0 @@
-package com.mockito;
-
-public class AddService {
-    public int getAddition(int a, int b) {
-        return a + b;
-    }
-}
--- JUnit_Demo_Project/src/com/mockito/AdditionService.java
+++
@@ -1,7 +0,0 @@
-package com.mockito;
-
-public class AdditionService {
-	public int getAddition(int a,int b) {
-		return a+b;
-	}
-}
