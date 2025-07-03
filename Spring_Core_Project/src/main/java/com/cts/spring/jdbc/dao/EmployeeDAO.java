8e745f834451fe21130a4ba0b33fab8093df6612
Ashutosh Mishra <120199306+ASHUTOSHMISHRA-2002@users.noreply.github.com>
2025-07-03 12:06:20 +0530

    Delete Spring_Core_Project directory

--- Spring_Core_Project/src/main/java/com/cts/spring/jdbc/dao/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.cts.spring.jdbc.dao;
-
-import com.cts.spring.jdbc.bean.Employee;
-
-public interface EmployeeDAO {
-	public int save(Employee employee);
-	public int delete(int eid);
-	public int update(int eid,int esalary);
-}
